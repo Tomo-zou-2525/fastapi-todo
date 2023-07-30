418d377478be55940c3327ecf3b3ed383e859220
Tomoya Hayasaki <bathbook@TomoyanoMacBook-Air-2.local>
2023-07-30 17:07:10 +0900

    create router

---
+++ routers/task.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+# Taksのリスト閲覧
+@router.get("/tasks")
+async def list_tasks():
+    pass
+
+# タスクの追加
+@router.post("/tasks")
+async def create_tasks():
+    pass
+
+# タスクの更新
+@router.put("/tasks/{task_id}")
+async def update_task():
+    pass
+
+# タスクの削除
+@router.delete("/tasks/{task_id}")
+async def delete_task():
+    pass
