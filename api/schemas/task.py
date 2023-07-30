588bd115f5c31469ef7d415a5b74cac0795f4585
Tomoya Hayasaki <bathbook@TomoyanoMacBook-Air-2.local>
2023-07-30 18:19:45 +0900

    create api schemas

---
+++ api/schemas/task.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+# APIのスキーマ定義を作成
+class Task(BaseModel):
+    id: int
+    title: Optional[str] = Field(None, example="Unko")
+    done: bool = Field(False, description="完了フラグ")
